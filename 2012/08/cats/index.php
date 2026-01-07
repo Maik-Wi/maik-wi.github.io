9bca183b484080fb9ae1fe535b328ceb552156fe
maik-wi <commit@maik.io>
2026-01-07 11:34:20 +0000

    ♡

---
+++ 2012/08/cats/index.php
@@ -0,0 +1,114 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset='UTF-8'>
+	
+	<title>animierte Katzen</title>
+	
+	
+</head>
+
+<style>
+* { margin: 0; padding: 0; }
+
+#photos {
+   /* Prevent vertical gaps */
+   line-height: 0;
+
+   -webkit-column-count: 5;
+   -webkit-column-gap:   0px;
+   -moz-column-count:    5;
+   -moz-column-gap:      0px;
+   column-count:         5;
+   column-gap:           0px;
+
+}
+#photos img {
+  /* Just in case there are inline attributes */
+  width: 100% !important;
+  height: auto !important;
+}
+
+@media (max-width: 1200px) {
+  #photos {
+  -moz-column-count:    4;
+  -webkit-column-count: 4;
+  column-count:         4;
+  }
+}
+@media (max-width: 1000px) {
+  #photos {
+  -moz-column-count:    3;
+  -webkit-column-count: 3;
+  column-count:         3;
+  }
+}
+@media (max-width: 800px) {
+  #photos {
+  -moz-column-count:    2;
+  -webkit-column-count: 2;
+  column-count:         2;
+  }
+}
+@media (max-width: 400px) {
+  #photos {
+  -moz-column-count:    1;
+  -webkit-column-count: 1;
+  column-count:         1;
+  }
+}
+</style>
+
+<body>
+
+<section id="photos">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze000.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze001.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze002.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze003.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze004.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze005.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze006.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze007.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze008.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze009.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze010.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze011.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze012.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze013.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze014.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze015.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze016.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze017.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze018.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze019.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze020.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze021.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze022.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze023.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze024.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze025.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze026.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze027.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze028.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze029.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze030.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze031.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze032.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze033.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze034.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze035.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze036.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze037.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze038.gif" alt="Happy Cat-Day">
+<img src="http://buntepixel.org/wp-content/uploads/2012/08/cats/katze039.gif" alt="Happy Cat-Day">
+</section>
+	
+	<center><a href="http://buntepixel.org/2012/08/08/welt-katzen-tag/">original Beitrag</a></center>
+	
+</body>
+
+</html>
